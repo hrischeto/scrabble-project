fc3c8ea8b8b2a382877ec8826e44b3ec5f7446e7
Hristina Krumova <152891241+hrischeto@users.noreply.github.com>
2024-01-20 23:36:08 +0200

    add settings option

--- scrabble/scrabble.cpp
+++ scrabble/scrabble.cpp
@@ -19,17 +19,15 @@
 constexpr size_t SIZE = 100;
 constexpr size_t NUMBER_OF_VOWELS = 5;
 
-void menu(int& menuChoise){
+void menu(int& menuChoice){
 	std::cout << "1 Start a new game!" << std::endl;
-	std::cout << "\n  Settings\n" << std::endl;
-	std::cout << "2\t Letters preferences" << std::endl;
-	std::cout << "3\t Rounds preferences" << std::endl;
-	std::cout << "\n4 Add to dictionary" << std::endl;
-	std::cout << "\n5 Exit" << std::endl;
+	std::cout << "\n2 Settings" << std::endl;
+	std::cout << "\n3 Add to dictionary" << std::endl;
+	std::cout << "\n4 Exit" << std::endl;
 
 	std::cout << "\nChoose number of option from the menu:" << std::endl;
 
-	std::cin >> menuChoise;
+	std::cin >> menuChoice;
 }
 
 void print(const char* str)
@@ -57,9 +55,9 @@ void generateLetters(char* givenLetters, int numberOfLetters) {
 	
 	char vowelArr[NUMBER_OF_VOWELS +1] = {'a','e','i','o','u'};
 
-	srand(time(nullptr));//see if it works without that
+	srand(time(nullptr));
 
-	for (int i = 0;i < 3;i++) {//add more words to dict
+	for (int i = 0;i < 3;i++) {
 
 		givenLetters[i] = vowelArr[ rand() % NUMBER_OF_VOWELS];
 	}
@@ -97,16 +95,6 @@ bool linearSearch(char* givenLetters, char searched) {
 	return 0;
 }
 
-//char** creatematrix(int rows) {
-//	char** dictionary = new char*[rows];
-//
-//	for (int i = 0;i < rows;i++) {
-//		int cols;
-//		dictionary[i] = new char[cols + 1];
-//		dictionary[i][0] = cols;
-//	}
-//}
-
 bool compareWords(char* inputWord, char* wordFromDict) {
 
 	if (!inputWord || !wordFromDict)
@@ -124,6 +112,7 @@ bool compareWords(char* inputWord, char* wordFromDict) {
 		return 0;
 	return 1;
 }
+
 bool isWordInDict(char* inputWord, std::ifstream& dictionary) {
 
 	char wordFromDict[SIZE + 1];
@@ -181,10 +170,9 @@ void checkWord(char* inputWord, char* givenLetters, int& gamePoints, std::ifstre
 	return;
 }
 
-void playGame(std::ifstream& dictionary)
+void playGame(std::ifstream& dictionary, int numberOfLetters, int rounds)
 {
-	int rounds = 2;
-	int numberOfLetters = 20;
+	
 	char givenLetters[SIZE];
 	char inputWord[SIZE + 1];
 	int gamePoints = 0;
@@ -198,38 +186,91 @@ void playGame(std::ifstream& dictionary)
 
 		checkWord(inputWord, givenLetters, gamePoints, dictionary);
 
-		std::cout << "Your points so far are " << gamePoints << "!" << std::endl;
+		std::cout << "\nYour points so far are: " << gamePoints << "!" << std::endl;
 	}
 
-	std::cout << "GAME OVER!\n Your total points are " << gamePoints << "!" << std::endl;
-	std::cout << "Returning to menu."<<std::endl;
+	std::cout << "\nGAME OVER!\n\nGood job! Your total points are " << gamePoints << "." << std::endl;
+	std::cout << "\nReturning to menu."<<std::endl;
+
 }
 
+void settings(std::ifstream& dictionary, int numberOfLetters, int rounds)
+{
+	std::cout << "\tSETTINGS" << std::endl;
+	std::cout << "\n1 Letters Preferences" << std::endl;
+	std::cout << "\n2 Rounds Preferences" << std::endl;
+	std::cout << "\n3 Start game" << std::endl;
+
+	int settingsChoice;
+
+	std::cout << "\nEnter number of option:";
+	std::cin >> settingsChoice;
+
+	switch (settingsChoice) {
+	case 1:
+
+		std::cout << "\nCurrently you are playing with " << numberOfLetters << " letters." << std::endl;
+		std::cout << "With how many letters do you want to play? Enter a number between 1 and 26:" << std::endl;
+		std::cin >> numberOfLetters;
+
+		while (numberOfLetters < 1 || numberOfLetters>26)
+		{
+			std::cout << "Enter a valid number between 1 and 26." << std::endl;
+			std::cin >> numberOfLetters;
+		}
+
+		settings(dictionary,  numberOfLetters,  rounds);
+		break;
+	
+	case 2:
+		std::cout << "\nYou will play " << rounds << " rounds." << std::endl;
+		std::cout << "How many rounds do you want to play?" << std::endl;
+		std::cin >> rounds;
+
+		settings(dictionary, numberOfLetters, rounds);
+		break;
+
+	case 3:
+		playGame(dictionary, numberOfLetters, rounds);
+		break;
+}
+}
 void scrabble() {
 
 	std::ifstream dictionary;
 	dictionary.open("scrabble_dict.txt");
 	if (!dictionary.is_open()) {
-		std::cout << "error!\n";
+		std::cout << "Error!"<<  std::endl;
 		return;
 	}
 
-	int menuChoise;
-	menu(menuChoise);
+	int menuChoice;
+	menu(menuChoice);
 
-	switch (menuChoise) {
+	const int defaultNumber = 10;
+	int numberOfLetters = defaultNumber;
+	int rounds = defaultNumber;
 
-	case 1:
-		playGame(dictionary);
+	switch (menuChoice) {
+
+	case 1:		//start game
+
+		std::cout<<"START!" << std::endl;
+		playGame(dictionary, numberOfLetters, rounds);
+
 		break;
-	case 2:
+
+	case 2:		//settings
+
+		settings(dictionary, numberOfLetters, rounds);
+
 		break;
-	case 3:
+
+	case 3:		
 		break;
-	case 4:
-		break;
-	case 5:
-		break;
+	case 4:		//exit
+		return;
+	
 
 	}
 
